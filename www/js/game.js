5ef7a2201bcf80afbcca5f55950b48c9aaa45371
Misbahu Sharfuddeen Zubair School of Physical Sciences MSZubair Computing <MSZubair@uclan.ac.uk>
2020-04-23 16:50:56 +0100

    fourth commit

--- www/js/game.js
+++ www/js/game.js
@@ -6,13 +6,12 @@ class Game{
         this.playernum = playernum;
         this.players = []
         this.count = 0;
-        this.time = 20;
+        this.time = time;
         this.play = {'Random':this.playRandom.bind(this), 'Rounted': this.playRouted.bind(this)}
     }
 
     startGame(){
         this.showInstructions()
-        console.log(this)
     }
 
     restart(){
@@ -142,14 +141,10 @@ class Game{
     }
 
     playGame(){
-        if ( this.count < this.playernum){
-            let app = this.clear()
-            let next = document.createElement('button')
-            next.innerText = `${this.players[this.count].name} is ready`;
-            let myShapes = new Shapes(this.size);
-            let set = myShapes.setShapes()
-            console.log(set)
-            next.onclick = () => this.play[this.mode](set, this.players[this.count])
+        if (this.count < this.playernum){
+            const app = this.clear()
+            const next = Util.createElement('button', '', '', `${this.players[this.count].name} is ready`)        
+            next.onclick = () => this.play[this.mode](this.shapes, this.players[this.count])
             app.appendChild(next)
         }
     }
@@ -395,32 +390,5 @@ class Player{
 
     setScore(score){
         this.score = score;
-    }
-}
-
-class Util{
-    static setShapes(size){
-        let allShapes = this.shuffleArray(['flowers', 'square', 'triangle', 'diamond', 'pentagon', 'cross'])
-        let subset = allShapes.slice(0, size)
-        let set = []
-        subset.forEach((shape, index) => {
-            let svg = document.createElement('img')
-            svg.src = `img/${shape}.svg`
-            svg.className = 'image-SVG'
-            set.push({shape: shape, svg: svg})
-        });
-        return set
-    }
-
-    static shuffleArray(array){
-        let currentIndex = array.length, temporaryValue, randomIndex;
-        while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
     }
 }
--- www/js/newgame.js
+++ www/js/newgame.js
@@ -1,6 +1,4 @@
 const CreateNew = () => {
-    let app = document.querySelector('.app')
-    app.innerHTML = ""
     let [buttonSetupGame, GameSettings] = SetupGame()
     buttonSetupGame.onclick = (e) =>{
         e.preventDefault()
@@ -10,79 +8,46 @@ const CreateNew = () => {
 }
 
 const SplashScreen = () => {
-    LoadModel(()=> console.log('Loaded'))
-    let app = document.querySelector('.app')
-    app.innerHTML = ""
-    let title = document.createElement('h1')
-    title.innerText = 'Home Rocks'
-    title.className = 'heading-one'
+    LoadModel(()=> console.log('Model loaded'))
+    const app = Util.clear()
+    const title = Util.createElement('h1', '', 'heading-one', 'Home Rocks')
     app.appendChild(title)
     TestModel().then((result)=>{console.log(result)}).then(()=>CreateNew()).catch((err)=>{console.log(err);CreateNew()})
 }
 
-
 const SetupGame = () => {
-    let settings = {'selectmode':'Random', 'selectsize':6, 'selectplayers':1}
-    let form = document.createElement('form');
-    let frag = document.createDocumentFragment();
-
-    let text = document.createElement('h1')
-    text.className = 'heading-two'
-    text.innerText = "Choose Game Settings"
-
-    let div_text = document.createElement('div')
-    div_text.appendChild(text)
-    div_text.className ='heading-div'
-
-    frag.appendChild(div_text)
-    
-    let div_form =  document.createElement('div')
-    div_form.className ='form-div'
-
-    let selectMode = SelectOptions('selectmode', 'Select mode', ['Random', 'Routed'], settings)
-    frag.appendChild(selectMode)
-    let selectSize = SelectOptions('selectsize', 'Select number of shapes', [6,5,4,3], settings)
-    frag.appendChild(selectSize)
-
-    let selectPlayers = SelectOptions('selectplayers', 'Select number of players', [1,2,3], settings)
-    frag.appendChild(selectPlayers)
-
-    let buttonCreate = document.createElement('button')
-    buttonCreate.innerHTML="Create Game"
-    frag.appendChild(buttonCreate)
+    const app = Util.clear()
+    const settings = {'selectmode':'Random', 'selectsize':6, 'selectplayers':1}
+    const form = document.createElement('form');
+    const heading = Util.createElement('h1', '', 'heading-two', 'Choose Game Settings')
+    const div_text = Util.createElement('div', '', 'heading-div')
+    div_text.appendChild(heading)
+    const div_form =  Util.createElement('div', '', 'form-div')
+    const selectMode = SelectOptions('selectmode', 'Select mode', ['Random', 'Routed'], settings)
+    const selectSize = SelectOptions('selectsize', 'Select number of shapes', [6,5,4,3], settings)
+    const selectPlayers = SelectOptions('selectplayers', 'Select number of players', [1,2,3], settings)
+    const buttonCreate = Util.createElement('button','', "","Create Game")
+    const frag = document.createDocumentFragment();
+    Util.appendChildren(frag, [div_text, selectMode, selectSize, selectPlayers, buttonCreate])   
     form.appendChild(frag)
-
     div_form.appendChild(form)
-
-    document.querySelector('.app').appendChild(div_text)
-    document.querySelector('.app').appendChild(div_form)
-
+    Util.appendChildren(app, [div_text, div_form])
     return [buttonCreate, settings]
 }
 
 const SelectOptions = (name, label, values, settings) => {
-    let frag = document.createDocumentFragment()
-
-    let div = document.createElement('div')
-    div.className = "select-div"
-
-    let label_tag = document.createElement('label')
-    label_tag.setAttribute('for', name)
-    label_tag.innerHTML = `<h3 class = "form-label">${label}</h3>`
-    
-
-    let select = document.createElement('select')
-    select.id = name;
+    const frag = document.createDocumentFragment()
+    const div = Util.createElement('div','',  'select-div')
+    const label_tag = Util.createElement('label', '', '', `<h3 class = "form-label">${label}</h3>`)
+    label_tag.setAttribute('for', name)    
+    const select = Util.createElement('select', name, '', '')
     values.forEach(value => {
-        let opt = document.createElement('option')
+        const opt = Util.createElement('option','', '', value) 
         opt.setAttribute('value', value)
-        opt.innerHTML=value;
         frag.appendChild(opt)
     });
     select.onchange = ({target}) => settings[name]=target.value;
     select.appendChild(frag)
-
-    div.appendChild(label_tag)
-    div.appendChild(select)
+    Util.appendChildren(div, [label_tag, select])
     return div
 }
